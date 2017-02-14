1035ce4d37f7f91db5042b9572ad9e6eea4f41bd
Manuel Revilla <manrevlob@gmail.com>
2017-02-14 18:32:13 +0100

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  devise_for :users, controllers: {
+      sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    root :to => 'devise/sessions#new'
+  end
+
+end
