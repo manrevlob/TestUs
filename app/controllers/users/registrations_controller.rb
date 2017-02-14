2978ca1ecaff83cb4785de9d2a14d5ce5953cbbf
FValle <fravalpin@alum.us.es>
2017-02-14 19:09:00 +0100

    Creado controlador de welcome y cambiado el route dependiendo de si logueado o no

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
-#   before_action :configure_permitted_parameters, if: :devise_controller?
 
   # GET /resource/sign_up
   # def new
@@ -37,12 +36,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:active, :name, :surname, :username])
-    # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:active, :name, :surname, :username) }
   end
 
   # If you have extra params to permit, append them to the sanitizer.
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: {
-      sessions: 'users/sessions'
+      registrations: 'users/registrations'
   }
 
   devise_scope :user do
+    authenticated :user do
+      root 'welcome#index', as: :authenticated_root
+    end
     root :to => 'devise/sessions#new'
   end
+
+
 
 end
