fbca0b4baf7ed3ac145ea38297a075edcaab0d7c
FValle <fravalpin@alum.us.es>
2017-05-31 19:53:20 +0200

    Añadido sistema de permisos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_current_projectito, :set_locale
-
+  before_action :set_current_session, :set_locale
 
 
   def set_locale
@@ -9,22 +8,26 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options(options = {})
-    { locale: I18n.locale }.merge options
+    {locale: I18n.locale}.merge options
   end
 
-  def set_current_projectito
+  def set_current_session
     unless current_user.nil?
-      @projects_can_select = Project.joins(:assign).where('assigns.user_id' => current_user.id)
+      @projects_can_select ||= Project.joins(:assign).where('assigns.user_id' => current_user.id)
       unless @projects_can_select.nil?
         if session[:project].nil?
           @current_project = @projects_can_select.first
           session[:project] = @current_project
         else
-          @current_project =  Project.find(session[:project]["id"])
+          @current_project = Project.find(session[:project]["id"])
         end
       end
-    end
 
+      # current_user.manages ||= Manage.where(:role_id => current_user.role.id)
+      # if session[:permission].nil?
+      #   session[:permission] = Manage.where(:role_id =>  current_user.role.id)
+      # end
+    end
   end
 
   def set_current_project
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,49 @@
 class User < ApplicationRecord
+  include ActiveModel::Dirty
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable,
          :trackable, :validatable
 
+  # attr_accessor :manages
+
   # Relaciones
   belongs_to :role
   has_many :assign
+
+  after_initialize do |user|
+    unless user.role.nil?
+      @manages ||= Manage.where(:role_id => user.role.id)
+    end
+  end
+
+
+  def CanRead?(zone)
+    # @CanRead = Manage.where(:role_id => self.role.id, :zone_id => zone).select(:canRead)
+    # @manages.where(:zone_id => zone).select(:canRead).first
+    @manages.select { |manage| manage.zone_id == zone }.first[:canRead]
+  end
+
+  def CanCreate?(zone)
+    # @CanCreate ||= Manage.where(:role_id => self.role.id, :zone_id => zone).select(:canCreate)
+    @manages.select { |manage| manage.zone_id == zone }.first[:canCreate]
+  end
+
+  def CanUpdate?(zone)
+    # @CanUpdate ||= Manage.where(:role_id => self.role.id, :zone_id => zone).select(:canUpdate)
+    @manages.select { |manage| manage.zone_id == zone }.first[:canUpdate]
+  end
+
+  def CanDelete?(zone)
+    # @CanDelete ||= Manage.where(:role_id => self.role.id, :zone_id => zone).select(:canDelete)
+    @manages.select { |manage| manage.zone_id == zone }.first[:canDelete]
+  end
+
+
+  private
+
+  # def getManage
+  #   @manages ||= Manage.where(:role_id => self.role.id)
+  # end
 
 end
